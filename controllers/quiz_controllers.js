ef1177a5163ce22466d0324ae32c440edb400552
rdab <rdamian.alfonso@riseup.net>
2015-08-02 20:46:51 -0500

    Mecanismo para editar quizes

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -52,13 +52,34 @@ exports.new = function(req, res) {
   res.render('quizes/new', { quiz: quiz, errors: [] });
 };
 
-// GET /quizes/create
+// POST /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build(req.body.quiz);
 
   quiz.validate().then( function(err) {
     if (err){
       res.render('quizes/new', { quiz: quiz, errors: err.errors });
+    } else {
+      quiz.save({ fields: ["pregunta", "respuesta"] })
+      .then( function(){ res.redirect('/quizes') });
+    }
+  });
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz;
+  res.render('quizes/edit', { quiz: quiz, errors: [] })
+};
+
+// PUT /quizes/:id
+exports.update = function (req, res) {
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz.validate().then( function(err) {
+    if (err){
+      res.render('quizes/edit', { quiz: req.quiz, errors: err.errors });
     } else {
       quiz.save({ fields: ["pregunta", "respuesta"] })
       .then( function(){ res.redirect('/quizes') });
--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,16 @@ var quiz_controllers = require('../controllers/quiz_controllers');
 router.get('/', function(req, res) {
   res.render('index', { title: "Quiz", errors: [] });
 });
+router.get('/author', quiz_controllers.author);
 
+// quizes routes
 router.param('quizId', quiz_controllers.load);
-
 router.get('/quizes', quiz_controllers.index);
 router.get('/quizes/:quizId(\\d+)', quiz_controllers.show);
 router.get('/quizes/:quizId(\\d+)/answer', quiz_controllers.answer);
 router.get('/quizes/new', quiz_controllers.new);
 router.post('/quizes/create', quiz_controllers.create);
-router.get('/author', quiz_controllers.author);
+router.get('/quizes/:quizId(\\d+)/edit', quiz_controllers.edit);
+router.put('/quizes/:quizId(\\d+)', quiz_controllers.update);
 
 module.exports = router;
