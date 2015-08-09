9f4fc8f250002bed380c8f3cc3661ecccad3625d
rdab <rdamian.alfonso@riseup.net>
2015-08-09 11:52:55 -0500

    Creadas rutas para añadir comentarios

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-var quiz_controllers = require('../controllers/quiz_controllers');
+var quizControllers = require('../controllers/quiz_controllers');
+var commentControllers = require('../controllers/comment_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: "Quiz", errors: [] });
 });
-router.get('/author', quiz_controllers.author);
+router.get('/author', quizControllers.author);
 
 // quizes routes
-router.param('quizId', quiz_controllers.load);
-router.get('/quizes', quiz_controllers.index);
-router.get('/quizes/:quizId(\\d+)', quiz_controllers.show);
-router.get('/quizes/:quizId(\\d+)/answer', quiz_controllers.answer);
-router.get('/quizes/new', quiz_controllers.new);
-router.post('/quizes/create', quiz_controllers.create);
-router.get('/quizes/:quizId(\\d+)/edit', quiz_controllers.edit);
-router.put('/quizes/:quizId(\\d+)', quiz_controllers.update);
-router.delete('/quizes/:quizId(\\d+)', quiz_controllers.destroy);
+router.param('quizId', quizControllers.load);
+router.get('/quizes', quizControllers.index);
+router.get('/quizes/:quizId(\\d+)', quizControllers.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizControllers.answer);
+router.get('/quizes/new', quizControllers.new);
+router.post('/quizes/create', quizControllers.create);
+router.get('/quizes/:quizId(\\d+)/edit', quizControllers.edit);
+router.put('/quizes/:quizId(\\d+)', quizControllers.update);
+router.delete('/quizes/:quizId(\\d+)', quizControllers.destroy);
 
+// comments routes
+router.get('/quizes/:quizId(\\d+)/comments/new', commentControllers.new);
+router.post('/quizes/:quizId(\\d+)/comments', commentControllers.create)
 module.exports = router;
